aa33149bdc966cc08082335511de8f5efcc8c5d7
Bharathboy1 <86122926+Bharathboy1@users.noreply.github.com>
2021-07-02 20:05:00 +0530

    Add files via upload

---
+++ SaitamaRobot/modules/ale.py
@@ -0,0 +1,38 @@
+from typing import Optional
+
+import SaitamaRobot.modules.sql.rules_sql as sql
+from SaitamaRobot import dispatcher
+from SaitamaRobot.modules.helper_funcs.chat_status import user_admin
+from SaitamaRobot.modules.helper_funcs.string_handling import markdown_parser
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    Message,
+    ParseMode,
+    Update,
+    User,
+)
+from telegram.error import BadRequest
+from telegram.ext import CallbackContext, CommandHandler, Filters, run_async
+from telegram.utils.helpers import escape_markdown
+
+
+def tms(update, chat_id, from_pm=False):
+    bot = dispatcher.bot
+    user = update.effective_user  # type: Optional[User]
+    reply_msg = update.message.reply_to_message
+    
+    @run_async
+            if rules and reply_msg:
+        reply_msg.reply_text(
+            "Please click the button below to see the rules.",
+            reply_markup=InlineKeyboardMarkup(
+                [
+                    [
+                        InlineKeyboardButton(
+                            text="Rules", url=f"t.me/{bot.username}?start={chat_id}",
+                        ),
+                    ],
+                ],
+            ),
+        )
